f8ad539dc58c101584b7f12c9068237d19882c5b
TheEinfachFelix <the.einfach.felix@gmail.com>
2025-04-16 21:55:18 +0200

    jetzt mir Software serial

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "Sim800L_Lib\Sim800L.hpp"
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
