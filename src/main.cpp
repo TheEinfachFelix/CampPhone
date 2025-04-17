ec1c322afabc8c41ff215950c624306fcf8b7e76
TheEinfachFelix <the.einfach.felix@gmail.com>
2025-04-17 18:32:17 +0200

    demo is done

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,41 @@
 #include <Arduino.h>
 #include "Sim800L_Lib\Sim800L.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
 
+#define RX 46
+#define TX 9
+//#define RX 9
+//#define TX 46
+
+Sim800L GSM(RX, TX);
+
+ 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  GSM.Debug = true;
+  delay(4000);
+  Serial.begin(9600);
+  GSM.begin();    
+  String pin = "2250";
+  
+  if (GSM.setPIN(pin)) Serial.println("Pin set");
+  else 
+  {
+    do
+    {
+      Serial.println("Pin ERROR");
+      delay(1000);
+    } while (GSM.setPIN(pin));
+  }
+
+  Serial.println("OK lets Go!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //GSM.checkStatus();
+  if (GSM.getCallStatus() == 3) {
+    Serial.println("Incoming call...");
+    GSM.answerCall();
+  }
+  delay(300);
 }
